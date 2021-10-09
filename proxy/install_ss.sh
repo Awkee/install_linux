b3514c6f108c3bb93ba0a5d84efc8a0419900fb7
Awkee <next4nextjob@gmail.com>
2021-10-09 20:20:48 +0800

    update

--- proxy/install_ss.sh
+++ proxy/install_ss.sh
@@ -11,6 +11,14 @@ if [ "$uid" != "0" ] ; then
     exit 0
 fi
 
+conf_file="$HOME/.ss.conf"
+red='\e[91m'
+green='\e[92m'
+yellow='\e[93m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+
 INFO() {
     echo -e "$@" >&2
 }
@@ -24,9 +32,22 @@ install_bin() {
     if [ -f "/usr/bin/shadowsocks2-linux" ] ; then
         echo "已经安装成功： /usr/bin/shadowsocks2-linux"
         echo "你是不是已经安装过ss啦？"
-        exit 0
+        return 0
     else
-        wget -c https://github.com/shadowsocks/go-shadowsocks2/releases/download/v0.1.5/shadowsocks2-linux.gz
+        ver=$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/shadowsocks/go-shadowsocks2/releases | grep -m1 'tag_name' | cut -d\" -f4)
+        if [[ ! $ver ]]; then
+            echo
+            echo -e " $red获取 Shadowsocks-Go 最新版本失败!!!$none"
+            echo
+            echo -e " 请尝试执行如下命令: $green echo 'nameserver 8.8.8.8' >/etc/resolv.conf $none"
+            echo
+            echo " 然后再重新运行脚本...."
+            echo
+            exit 1
+        fi
+        _link="https://github.com/shadowsocks/go-shadowsocks2/releases/download/$ver/shadowsocks2-linux.gz"
+
+        wget -c ${_link}
         gzip -d shadowsocks2-linux.gz
         chmod +x shadowsocks2-linux
         mv shadowsocks2-linux /usr/bin/
@@ -34,203 +55,6 @@ install_bin() {
     fi
 }
 
-uninstall() {
-    port="$1"
-    echo "走到这一步，说明你再也不想使用ss了！你确定要这样么？(按任意键继续，Ctrl+C 取消操作):"
-    read your_answer
-    if [ "$port" = "c" -o "$port" = "client" ] ; then
-        echo "客户端清理工作"
-        systemctl disable --now ssclient.service
-        rm -f /usr/bin/shadowsocks2-linux /usr/bin/ss2go
-        rm -f /usr/lib/systemd/system/ssclient.service
-        systemctl daemon-reload
-        exit 0
-    fi
-    systemctl disable --now ssserver.service
-    rm -f /usr/bin/shadowsocks2-linux /usr/bin/ss2go
-    rm -f /usr/lib/systemd/system/ssserver.service
-    systemctl daemon-reload
-    del_firewall ${port}
-}
-
-# 添加systemd服务
-add_service() {
-    cmd="$1"
-    fn="$2"
-    if [ -f "$fn" ] ; then
-        echo "文件已经存在！如果你的确想要重新安装，请先手工删除 $fn 文件."
-        exit 1
-    fi
-    cat <<END > ${fn}
-[Unit]
-Description=Shadowsocks2
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-User=nobody
-PermissionsStartOnly=true
-ExecStart=${cmd}
-Nice=-10
-KillMode=process
-Restart=on-failure
-ProtectSystem=full
-ProtectHome=true
-PrivateTmp=true
-PrivateDevices=true
-NoNewPrivileges=true
-
-[Install]
-WantedBy=multi-user.target
-
-END
-
-}
-
-# 添加防火墙配置
-add_firewall() {
-    echo "开始添加 iptables 防火墙配置"
-    ss_port="${1:-12345}"
-    maxlink="50"
-    iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT                #允许已建立的或相关连的通行
-    iptables -A INPUT -p tcp -m multiport --dport 22,80,443,${ss_port} -j ACCEPT    # 端口开放
-    # 限制每个客户端的HTTPS服务连接数
-    iptables -A INPUT -p tcp --syn --dport ${ss_port} -m connlimit --connlimit-above ${maxlink} -j REJECT
-    iptables -A INPUT -p tcp --syn --dport ${ss_port} -m connlimit --connlimit-upto ${maxlink}  -j ACCEPT
-    iptables -A INPUT -j DROP   #禁止其他未允许的规则访问
-
-    iptables -A FORWARD -p TCP ! --syn -m state --state NEW -j DROP                 # 丢弃异常的TCP连接包(可以屏蔽ACK扫描)
-    iptables -A FORWARD -p icmp -j DROP                                             # 禁止ICMP包
-    echo "防火墙规则："
-    echo "  开放端口:22,80,443,${ss_port}"
-    echo "  限制单个IP最大连接数:${maxlink}"
-    echo "  丢弃所有ICMP消息"
-    echo "  丢弃其他外来消息包"
-}
-
-del_firewall() {
-    echo "清理 iptables 防火墙配置"
-    ss_port="${1:-12345}"
-    maxlink="50"
-    iptables-save > /tmp/iptables.bak
-    iptables -D INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT                #允许已建立的或相关连的通行
-    iptables -D INPUT -p tcp -m multiport --dport 22,80,443,${ss_port} -j ACCEPT    # 端口开放
-    # 限制每个客户端的HTTPS服务连接数
-    iptables -D INPUT -p tcp --syn --dport ${ss_port} -m connlimit --connlimit-above ${maxlink} -j REJECT
-    iptables -D INPUT -p tcp --syn --dport ${ss_port} -m connlimit --connlimit-upto ${maxlink}  -j ACCEPT
-    iptables -D INPUT -j DROP   #禁止其他未允许的规则访问
-
-    iptables -D FORWARD -p TCP ! --syn -m state --state NEW -j DROP                 # 丢弃异常的TCP连接包(可以屏蔽ACK扫描)
-    iptables -D FORWARD -p icmp -j DROP                                             # 禁止ICMP包
-}
-
-select_cipher(){
-    # 根据提示选择加密方法
-    cipher_list="AEAD_CHACHA20_POLY1305 AEAD_AES_256_GCM AEAD_AES_128_GCM"
-    i=1
-    for cc in ${cipher_list}
-    do
-        choice[$i]="$cc"
-        LOG "$i : $cc"
-        i=`expr $i + 1`
-    done
-    LOG "选择加密方法：(默认:1)"
-    read your_answer
-    if [ "$your_answer" = "" ] ; then
-        LOG "您选择使用默认值:1"
-        your_answer="1"
-    elif [ "$your_answer" -le "0" -a "$your_answer" -ge "$i" ] ; then
-        LOG "输入值不再范围，已帮您选择使用默认值:1"
-        your_answer="1"
-    fi
-    res=${choice[$your_answer]}
-    LOG "使用加密方式： ${res}"
-    unset choice
-    echo "$res"
-}
-
-select_port() {
-    LOG "设置服务端端口（1024-65535)：(默认:12345)"
-    read your_answer
-    if [ "$your_answer" = "" ] ; then
-        your_answer="12345"
-        LOG "使用默认值:${your_answer}"
-    elif [ "$your_answer" -le "1024" -o "$your_answer" -ge "65535" ] ; then
-        your_answer="12345"
-        LOG "无效输入！已帮您选择默认值:$your_answer"
-    fi
-    echo "$your_answer"
-}
-
-# 设置密码
-select_pass() {
-    rand_pass=`echo "$(openssl rand -hex 32)$(date +%s%N)" | md5sum|base64|md5sum | awk '{ print $1}'`
-    
-    LOG "随机密码算法(随机序列+系统时间值):" "echo \"$(openssl rand -hex 32)$(date +%s%N)\" | md5sum | awk '{ print $1}'"
-    LOG "请设置代理密码：(直接回车取默认的随机值:" "${rand_pass}"
-    read your_answer
-    if [ "$your_answer" = "" ] ; then
-        LOG "您选择使用默认值:" "${rand_pass}"
-        your_answer="${rand_pass}"
-    else
-        LOG "自定义密码：" "${your_answer}"
-    fi
-    echo "$your_answer"
-}
-
-# 安装服务端
-install_server() {
-    
-    # 安装可执行程序
-    install_bin
-
-    # 简单启动命令: 格式为'ss://<cipher_method>:<your_password>@:<your_port>'
-    # 想了解AEAD加密方法,请阅读 https://shadowsocks.org/en/wiki/AEAD-Ciphers.html
-    
-    random_password=`select_pass`
-    cipher=`select_cipher`
-    server_ip=$(ifconfig eth0 | awk '/inet /{ print $2 }')
-    server_port=`select_port`
-
-    ss_uri="ss://${cipher}:${random_password}@${server_ip}:${server_port}"
-    echo "客户端/服务端使用的SS链接: ${ss_uri}"
-
-    # 添加自启动服务
-    add_service "ss2go -s '${ss_uri}'" "/usr/lib/systemd/system/ssserver.service"
-    # 启动服务端进程
-    echo "开始启动服务端进程"
-    systemctl enable --now ssserver.service
-    systemctl status ssserver.service
-
-    add_firewall ${server_port}
-    enable_bbr
-    info
-}
-
-# 安装客户端
-install_client() {
-    SS_URI="$1"
-    local_port="${2:-1080}"
-    if [ "$SS_URI" = "" ] ; then
-        echo "SS_URI 参数没设置!"
-        return 1
-    fi
-    if [ "${SS_URI:0:5}" != "ss://" ] ; then
-        echo "SS_URI 前缀不是以 ss://开头!你确定没搞错？！？"
-        return 2
-    fi
-    install_bin
-    add_service "ss2go -c '$SS_URI' -socks :${local_port}" "/usr/lib/systemd/system/ssclient.service"
-
-    echo "开始启动客户端进程"
-    systemctl enable --now ssclient.service
-
-    systemctl status ssclient.service
-}
-
-get_uri(){
-    echo "`awk '/^ExecStart/{ print $3 }' $1 | sed 's/'\''//g'`"
-}
 
 cmd_install() {
     # 系统命令安装 apt/dnf/zypper/pacman
@@ -255,24 +79,359 @@ cmd_install() {
     return
 }
 
+# 添加systemd服务
+add_service() {
+    cmd="$1"
+    service_name="$2"
+    while ：
+    do
+        if [ "$service_name" = "" ] ; then
+            read -p "输入新服务名(例如:ss2go01,或回车退出):" your_answer
+            if [ "$your_answer" = "" ] ; then
+                LOG "退出!"
+                exit 0
+            fi
+            service_name="$your_answer"
+        fi
+        fn="/usr/lib/systemd/system/${service_name}.service"
+        if [ -f "$fn" ] ; then
+            read -p "服务文件[${fn}]已经存在！输入个新服务名(例如:ss2go01,或回车退出)?" your_answer
+            if [ "${your_answer}" = "" ] ; then
+                exit 0
+            fi
+            service_name="${your_answer}"
+        fi
+        break
+    done
+    cat <<END > ${fn}
+[Unit]
+Description=Shadowsocks2
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=nobody
+PermissionsStartOnly=true
+ExecStart=${cmd}
+Nice=-10
+KillMode=process
+Restart=on-failure
+ProtectSystem=full
+ProtectHome=true
+PrivateTmp=true
+PrivateDevices=true
+NoNewPrivileges=true
+
+[Install]
+WantedBy=multi-user.target
+
+END
+
+    # 启动服务端进程
+    echo "启动[${service_name}]服务进程"
+    systemctl daemon-reload
+    systemctl enable --now ${service_name}.service
+    systemctl status ${service_name}.service
+    echo "${service_name}" >> ${conf_file}
+    if [ "$?" != "0" ] ; then
+        echo "服务启动失败啦！尝试根据错误信息解决问题吧！修复后再执行 systemctl start ${service_name} 启动试试。"
+    fi
+}
+
+# 初始化防火墙配置
+init_firewall() {
+    echo "开始添加 iptables 防火墙配置"
+    maxlink="150"
+    iptables -A INPUT -p tcp -m multiport --dport 22,80,443 -j ACCEPT    # 端口开放
+    iptables -A INPUT -j DROP   #禁止其他未允许的规则访问
+
+    iptables -A FORWARD -p TCP ! --syn -m state --state NEW -j DROP                 # 丢弃异常的TCP连接包(可以屏蔽ACK扫描)
+    iptables -A FORWARD -p icmp -j DROP                                             # 禁止ICMP包
+    echo "防火墙规则："
+    echo "  开放端口:22,80,443"
+    echo "  丢弃所有ICMP消息"
+    echo "  丢弃其他外来消息包"
+}
+
+del_firewall() {
+    echo "清理 iptables 防火墙配置"
+    maxlink="150"
+    bak_file="/tmp/iptables.`date +%Y%m%d%H%M%S`"
+    iptables-save > ${bak_file}
+    echo "已经备份iptables规则到文件[$bak_file]中！"
+    iptables -D INPUT -p tcp -m multiport --dport 22,80,443 -j ACCEPT    # 端口开放
+    iptables -D INPUT -j DROP   #禁止其他未允许的规则访问
+
+    iptables -D FORWARD -p TCP ! --syn -m state --state NEW -j DROP                 # 丢弃异常的TCP连接包(可以屏蔽ACK扫描)
+    iptables -D FORWARD -p icmp -j DROP                                             # 禁止ICMP包
+}
+
+# 添加防火墙端口
+add_port() {
+    echo "开始添加 iptables 防火墙配置"
+    ss_port="${1:-12345}"
+    if netstat -utlnp | grep "${ss_port}" | grep -v grep > /dev/null ; then 
+        LOG "${ss_port}" "端口监听中"
+    else
+        LOG "这个端口没有服务呢？" "记得等下手工启动一下这个服务吧！"
+    fi
+    maxlink="150"
+    # 开放端口
+    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport ${ss_port} -j ACCEPT
+    iptables -I INPUT -m state --state NEW -m udp -p udp --dport ${ss_port} -j ACCEPT
+    # 限制每个客户端的HTTPS服务连接数
+    iptables -I INPUT -p tcp --syn --dport ${ss_port} -m connlimit --connlimit-above ${maxlink} -j REJECT
+    iptables -I INPUT -p tcp --syn --dport ${ss_port} -m connlimit --connlimit-upto ${maxlink}  -j ACCEPT
+}
+
+# 删除防火墙端口
+del_port() {
+    echo "开始删除 iptables 防火墙端口配置"
+    ss_port="${1:-12345}"
+    if netstat -utlnp | grep "${ss_port}" | grep -v grep > /dev/null ; then 
+        LOG "${ss_port}端口监听中," "记得等下手工停止掉这个服务吧！"
+    else
+        LOG "[${ss_port}]" "端口没有服务了!"
+    fi
+    maxlink="150"
+    # 开放端口
+    iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport ${ss_port} -j ACCEPT
+    iptables -D INPUT -m state --state NEW -m udp -p udp --dport ${ss_port} -j ACCEPT
+    # 限制每个客户端的HTTPS服务连接数
+    iptables -D INPUT -p tcp --syn --dport ${ss_port} -m connlimit --connlimit-above ${maxlink} -j REJECT
+    iptables -D INPUT -p tcp --syn --dport ${ss_port} -m connlimit --connlimit-upto ${maxlink}  -j ACCEPT
+}
+
+ciphers=(
+	aes-128-cfb
+	aes-192-cfb
+	aes-256-cfb
+	aes-128-ctr
+	aes-192-ctr
+	aes-256-ctr
+	aes-128-gcm
+	aes-192-gcm
+	aes-256-gcm
+	xchacha20
+	chacha20-ietf
+	chacha20-ietf-poly1305
+    #AEAD_CHACHA20_POLY1305
+    #AEAD_AES_256_GCM
+    #AEAD_AES_128_GCM
+)
+
+select_cipher(){
+    # 根据提示选择加密方法
+    i=1
+    for ((i = 1; i <= ${#ciphers[*]}; i++)); do
+        echo
+        if [[ "$i" -le 9 ]]; then
+            LOG " $i. ${ciphers[$i - 1]}"
+        else
+            LOG "$i. ${ciphers[$i - 1]}"
+        fi
+    done
+    read -p "$(echo -e 选择加密方法(默认:1)):" your_answer
+    if [ "$your_answer" = "" ] ; then
+        LOG "您选择使用默认值:1"
+        your_answer="1"
+    elif [ "$your_answer" -le "0" -a "$your_answer" -ge "$i" ] ; then
+        LOG "输入值不再范围，已帮您选择使用默认值:1"
+        your_answer="1"
+    fi
+    res=${choice[$your_answer]}
+    LOG "使用加密方式：" "[${res}]"
+    echo "$res"
+}
+
+select_port() {
+    while :
+    do
+        LOG "设置服务端口（1024-65535)："
+        read -p "(默认:12345):" your_answer
+        if [ "$your_answer" = "" ] ; then
+            your_answer="12345"
+            LOG "使用默认值:${your_answer}"
+        elif [ "$your_answer" -le "1024" -o "$your_answer" -ge "65535" ] ; then
+            your_answer="12345"
+            LOG "无效输入！已帮您选择默认值:$your_answer"
+        fi
+        if netstat -tanp|grep LISTEN |grep ":${your_answer}" ; then
+            LOG "端口检测已经被占用! 重新选一个吧！"
+        else
+            break
+        fi
+    done
+    echo "$your_answer"
+}
+
+function random_string_gen() {
+    PASS=""
+    MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_+" # "~!@#$%^&*()_+="
+    LENGTH=$1
+    [ -z $1 ] && LENGTH="32"
+    while [ "${n:=1}" -le "$LENGTH" ]
+    do
+        PASS="$PASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+        let n+=1
+    done
+
+    echo ${PASS}
+}
+
+# 设置密码
+select_pass() {
+    rand_pass=`random_string_gen`
+    
+    LOG "设置密码：(默认值${rand_pass}):"
+    read your_answer
+    if [ "$your_answer" = "" ] ; then
+        LOG "您选择使用默认值:" "${rand_pass}"
+        your_answer="${rand_pass}"
+    else
+        LOG "自定义密码：" "${your_answer}"
+    fi
+    echo "$your_answer"
+}
+
+# 安装服务端
+install_server() {
+    # 安装可执行程序
+    install_bin
+
+    # 简单启动命令: 格式为'ss://<cipher_method>:<your_password>@:<your_port>'
+    # 想了解AEAD加密方法,请阅读 https://shadowsocks.org/en/wiki/AEAD-Ciphers.html
+    
+    add_server
+
+    enable_bbr
+    info
+}
+
+# 添加新服务
+add_server(){
+    random_password=`select_pass`
+    cipher=`select_cipher`
+    server_ip=$(ifconfig eth0 | awk '/inet /{ print $2 }')
+    server_port=`select_port`
+
+    ss_uri="ss://${cipher}:${random_password}@${server_ip}:${server_port}"
+    echo "客户端/服务端使用的SS链接: ${ss_uri}"
+
+    # 添加自启动服务
+    read -p "输入新服务名(例如:ss2go01,或回车退出):" your_answer
+    if [ "$your_answer" = "" ] ; then
+        LOG "退出!"
+        exit 0
+    fi
+    service_name="$your_answer"
+    add_service "ss2go -s '${ss_uri}'" "${service_name}"
+    add_firewall ${server_port}
+
+}
+
+# 删除服务
+del_server(){
+    service_name="$1"
+    if grep "$service_name" ${conf_file} >/dev/null ; then
+        # 服务存在
+        if systemctl is-active ${service_name} ; then
+            systemctl disable --now ${service_name}
+            systemctl status ${service_name}
+            sport=`systemctl cat ssserver.service| sed 's/'\''//g' | awk -F: '/^ExecStart/{print $NF }'`
+            del_port ${sport}
+        fi
+        sed -i "/${service_name}/d" ${conf_file}
+    fi
+}
+
+# 删除所有服务端ss服务
+del_all_server(){
+    cat ${conf_file} | while read sn
+    do
+        if systemctl is-active ${service_name} ; then
+            systemctl disable --now ${service_name}
+            systemctl status ${service_name}
+            sport=`systemctl cat ssserver.service| sed 's/'\''//g' | awk -F: '/^ExecStart/{print $NF }'`
+            del_port ${sport}
+        fi
+    done
+    rm -f ${conf_file}
+}
+
+# 服务信息查看
+service_status(){
+    if [ "$1" != "" ] ; then
+        # 指定单个服务查询
+        info "$1"
+        return 0
+    fi
+    # 所有服务查询
+    cat ${conf_file} | while read sn
+    do
+        LOG "服务：${sn}信息:"
+        systemctl status ${sn}
+        if [ "$?" = "0" ] ; then 
+            info ${sn}
+        else
+            LOG "服务: $sn 未启动！"
+        fi
+    done
+        
+}
+# 安装客户端
+install_client() {
+    SS_URI="$1"
+    local_port="${2:-1080}"
+    if [ "$SS_URI" = "" ] ; then
+        echo "SS_URI 参数没设置!"
+        return 1
+    fi
+    if [ "${SS_URI:0:5}" != "ss://" ] ; then
+        echo "SS_URI 前缀不是以 ss://开头!你确定没搞错？！？"
+        return 2
+    fi
+    install_bin
+    add_service "ss2go -c '$SS_URI' -socks :${local_port}" "ssclient"
+}
+
+uninstall() {
+    service_name="$1"
+    echo "走到这一步，说明你再也不想使用ss了！你确定要这样么？(按任意键继续，Ctrl+C 取消操作):"
+    read _nouse
+    if [ "$service_name" = "client" ] ; then
+        echo "客户端清理工作"
+        systemctl disable --now ssclient.service
+        rm -f /usr/bin/shadowsocks2-linux /usr/bin/ss2go
+        rm -f /usr/lib/systemd/system/ssclient.service
+        systemctl daemon-reload
+        exit 0
+    fi
+    if [ "$service_name" = "" ] ; then
+        del_all_server
+        return 0
+    fi
+    del_server ${service_name}
+}
+
+get_uri(){
+    # 提取服务配置中的URI信息
+    echo "`awk '/^ExecStart/{ print $3 }' $1 | sed 's/'\''//g'`"
+}
+
 info() {
-    if [ -f "/usr/lib/systemd/system/ssserver.service" ] ; then
-        uri_str=`get_uri /usr/lib/systemd/system/ssserver.service`
+    service_name=${1:-"ssserver"}
+    if [ -f "/usr/lib/systemd/system/${service_name}.service" ] ; then
+        uri_str=`get_uri /usr/lib/systemd/system/${service_name}.service`
     else
         uri_str=`get_uri /usr/lib/systemd/system/ssclient.service`
     fi
     INFO "========================================================================"
     LOG "SS原始链接（ss-go客户端配置用）：" ${uri_str}
     params=`echo ${uri_str:5}`
-    aead_method=`echo $params| cut -d: -f1`
-    method=`echo ${aead_method} | tr 'A-Z' 'a-z' |sed 's/aead_//'`
-    new_uri=`echo -e "$uri_str\c" |sed "s/${aead_method}/${method}/"`
-    share_uri_aead="ss://`echo -e "${uri_str:5}\c" |base64 -w0`#ss01"
-    LOG "分享 支持AEAD加密算法链接 移动APP使用：" "${share_uri_aead}" 
-    share_uri="ss://`echo -e "${new_uri:5}\c" |base64 -w0`#ss01"
-    LOG "分享 不支持AEAD加密算法链接 移动APP用：" "${share_uri}"
+    share_uri="ss://`echo -e "${params}\c" |base64 -w0`"
+    LOG "分享SS链接：" "${share_uri}"
     LOG "分享移动APP使用链接的二维码："
-    INFO "========================================================================"
     if ! qrencode -h  >/dev/null 2>&1 ; then 
         LOG "稍等一下..." "正在为您安装qrencode二维码生成工具!"
         cmd_install qrencode
@@ -281,7 +440,8 @@ info() {
         LOG "糟糕！" "您的系统安装 qrencode 失败!" 
         return 1
     fi
-    qrencode -s6 -l L -t UTF8 -o - ${share_uri}
+    qrencode -m2 -l L -t UTF8 -o - ${share_uri}
+    INFO "========================================================================"
 }
 
 init_config() {
@@ -317,32 +477,37 @@ usage() {
     cat <<END
 Usage:
     `basename $0` server                        # 第一次安装服务端
+    `basename $0` add                           # 服务端安装新服务
+    `basename $0` uninstall                     # 卸载服务端安装的所有ss服务及相关软件
+    `basename $0` uninstall [service]           # 卸载服务端安装的单个服务,service为服务名称
+
     `basename $0` client uri  port              # 第一次安装客户端
-    `basename $0` uninstall                     # 卸载服务端安装软件及配置，默认端口12345
-    `basename $0` uninstall  port               # 卸载服务端安装软件及配置，自定义端口port
-    `basename $0` uninstall  client             # 卸载客户端安装软件及配置
-    `basename $0` enable_bbr                    # 启用BBR加速(服务端第一次安装自动开启)
-    `basename $0` info                          # 查看SS链接URI信息
+    `basename $0` remove                        # 卸载客户端安装软件及配置
+
+    `basename $0` status [service]              # 查看SS链接URI信息(默认service名为ssserver或ssclient)
 END
 }
 
 case "$1" in
-    server|s)
-    install_server
-    ;;
-    client|c)
-    install_client $2 $3
-    ;;
-    uninstall|uni)
-    uninstall $2
-    ;;
-    enable_bbr)
-    $1
-    ;;
-    info|i)
-    info
-    ;;
+    server)
+        install_server
+        ;;
+    add)
+        add_server
+        ;;
+    status)
+        service_status $2
+        ;;
+    client)
+        install_client $2 $3
+        ;;
+    uninstall)
+        uninstall $2
+        ;;
+    remove)
+        uninstall client
+        ;;
     *)
-    usage
-    ;;
+        usage
+        ;;
 esac
